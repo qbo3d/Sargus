acefd01f4df29ff626d488a95a531030e40944e8
Sergio Mesa <media@qbo3d.com>
2019-04-04 22:55:06 -0500

    Fracaso con Login

--- Flutter/sargus/lib/main.dart
+++ Flutter/sargus/lib/main.dart
@@ -1,36 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sargus/ui/login_page.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'TheGorgeousLogin',
+      theme: new ThemeData(
+
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: new Text('Body'),
+      home: new LoginPage(),
     );
   }
 }
